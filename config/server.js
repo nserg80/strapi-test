e69b48fbeb209f064b72b8179ad4296da62d659b
Serhii Nyzhnyk <serhii.ny@maxitechs.com>
2020-11-10 15:04:56 +0200

    inital

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6308c18a43a386eaddd7cc6244fec484'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '40fb7fb9-46a1-44ee-8337-3bd5dd441d71'
+};
